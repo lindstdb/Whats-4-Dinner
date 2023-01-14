471319254999b62ad830db30a391cdaee5b8d270
lindstdb <115056565+lindstdb@users.noreply.github.com>
2023-01-13 22:36:12 -0600

    adds funtionality to vegetarian button

--- dillon/Assets/js/script.js
+++ dillon/Assets/js/script.js
@@ -1,19 +1,25 @@
 // Add event listener to button
-document.getElementById("generate").addEventListener("click", fetchRandomRecipeAndRedirect);
+document.getElementById("generate").addEventListener("click", fetchRandomRecipeAndWinePairingAndRedirect);
 
-async function fetchRandomRecipeAndRedirect() {
+async function fetchRandomRecipeAndWinePairingAndRedirect() {
   const apiKey = "39d72333b5944dc0b8267b56d752f579";
-  const url = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}`;
+  const url = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&includeWinePairing=true`;
 
   try {
     const response = await fetch(url);
     const recipeData = await response.json();
     const recipeTitle = recipeData.title;
+    const recipeSourceURL = recipeData.sourceUrl;
     const recipeImage = recipeData.image;
+    const wineName = recipeData.winePairing.pairedWines[0];
+    const wineImage = recipeData.winePairing.pairingText;
 
-    // Store the recipe data in session storage
+    // Store the recipe and wine pairing data in session storage
     sessionStorage.setItem("recipeTitle", recipeTitle);
+    sessionStorage.setItem("recipeSourceURL", recipeSourceURL);
     sessionStorage.setItem("recipeImage", recipeImage);
+    sessionStorage.setItem("wineName", wineName);
+    sessionStorage.setItem("wineImage", wineImage);
 
     // Redirect to TJindex.html
     window.location.href = "TJindex.html";
@@ -27,17 +33,23 @@ document.getElementById("keto").addEventListener("click", fetchRandomKetoRecipeA
 
 async function fetchRandomKetoRecipeAndRedirect() {
   const apiKey = "39d72333b5944dc0b8267b56d752f579";
-  const url = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&diet=ketogenic`;
+  const url = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&diet=ketogenic&maxWinePairing=1`;
 
   try {
     const response = await fetch(url);
     const recipeData = await response.json();
     const recipeTitle = recipeData.title;
     const recipeImage = recipeData.image;
+    const recipeSourceUrl = recipeData.sourceUrl;
+    const wineName = recipeData.winePairing.pairedWines[0];
+    const wineImage = recipeData.winePairing.productMatches[0].imageUrl;
 
-    // Store the recipe data in session storage
+    // Store the recipe and wine data in session storage
     sessionStorage.setItem("recipeTitle", recipeTitle);
     sessionStorage.setItem("recipeImage", recipeImage);
+    sessionStorage.setItem("recipeSourceUrl", recipeSourceUrl);
+    sessionStorage.setItem("wineName", wineName);
+    sessionStorage.setItem("wineImage", wineImage);
 
     // Redirect to TJindex.html
     window.location.href = "TJindex.html";
@@ -51,17 +63,53 @@ document.getElementById("kosher").addEventListener("click", fetchRandomKosherRec
 
 async function fetchRandomKosherRecipeAndRedirect() {
   const apiKey = "39d72333b5944dc0b8267b56d752f579";
-  const url = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&diet=kosher`;
+  const url = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&diet=kosher&maxWinePairing=1`;
 
   try {
     const response = await fetch(url);
     const recipeData = await response.json();
     const recipeTitle = recipeData.title;
     const recipeImage = recipeData.image;
+    const recipeSourceUrl = recipeData.sourceUrl;
+    const wineName = recipeData.winePairing.pairedWines[0];
+    const wineImage = recipeData.winePairing.productMatches[0].imageUrl;
 
-    // Store the recipe data in session storage
+    // Store the recipe and wine data in session storage
     sessionStorage.setItem("recipeTitle", recipeTitle);
     sessionStorage.setItem("recipeImage", recipeImage);
+    sessionStorage.setItem("recipeSourceUrl", recipeSourceUrl);
+    sessionStorage.setItem("wineName", wineName);
+    sessionStorage.setItem("wineImage", wineImage);
+
+    // Redirect to TJindex.html
+    window.location.href = "TJindex.html";
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Add event listener to button
+document.getElementById("vegetarian").addEventListener("click", fetchRandomVegetarianRecipeAndRedirect);
+
+async function fetchRandomVegetarianRecipeAndRedirect() {
+  const apiKey = "39d72333b5944dc0b8267b56d752f579";
+  const url = `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&diet=vegetarian&maxWinePairing=1`;
+
+  try {
+    const response = await fetch(url);
+    const recipeData = await response.json();
+    const recipeTitle = recipeData.title;
+    const recipeImage = recipeData.image;
+    const recipeSourceUrl = recipeData.sourceUrl;
+    const wineName = recipeData.winePairing.pairedWines[0];
+    const wineImage = recipeData.winePairing.productMatches[0].imageUrl;
+
+    // Store the recipe and wine data in session storage
+    sessionStorage.setItem("recipeTitle", recipeTitle);
+    sessionStorage.setItem("recipeImage", recipeImage);
+    sessionStorage.setItem("recipeSourceUrl", recipeSourceUrl);
+    sessionStorage.setItem("wineName", wineName);
+    sessionStorage.setItem("wineImage", wineImage);
 
     // Redirect to TJindex.html
     window.location.href = "TJindex.html";
