fbfe27e7af863b8c7431d8ab49bc473b277def79
TJ Momon <tjmomon@TJs-MacBook-Air.local>
2023-01-16 21:23:01 -0600

    commented out expand javascript; not sure if needed

--- TJ/assets/js/script.js
+++ TJ/assets/js/script.js
@@ -20,31 +20,31 @@ var recipeSourceUrl = localStorage.getItem("recipeSourceUrl");
 // Populate the foodRecipe div with the recipe data
 document.getElementById("card-populate").innerHTML = "<h4>" + recipeTitle + "</h4><img src='" + recipeImage + "'><a href='" + recipeSourceUrl + "'>Go To Recipe</a>";
 
-function expandCard() {
-    var checkbox = document.getElementById('expand');
-    var a = document.getElementById('foodRecipe');
-    var b = document.getElementById('drinkRecipe');
-    if (checkbox.checked == true) {
-        a.className = "expandCard";
-        b.className = "hide";
-    } else if (checkbox.checked == !true) {
-        a.className = "card";
-        b.className = "card";
-    }
-};
+// function expandCard() {
+//     var checkbox = document.getElementById('expand');
+//     var a = document.getElementById('foodRecipe');
+//     var b = document.getElementById('drinkRecipe');
+//     if (checkbox.checked == true) {
+//         a.className = "expandCard";
+//         b.className = "hide";
+//     } else if (checkbox.checked == !true) {
+//         a.className = "card";
+//         b.className = "card";
+//     }
+// };
 
-function expandCard2() {
-    var checkbox2 = document.getElementById('expand2');
-    var a = document.getElementById('foodRecipe');
-    var b = document.getElementById('drinkRecipe');
-    if (checkbox2.checked == true) {
-        b.className = "expandCard";
-        a.className = "hide";
-    } else if (checkbox2.checked == !true) {
-        b.className = "card";
-        a.className = "card";
-    }
-};
+// function expandCard2() {
+//     var checkbox2 = document.getElementById('expand2');
+//     var a = document.getElementById('foodRecipe');
+//     var b = document.getElementById('drinkRecipe');
+//     if (checkbox2.checked == true) {
+//         b.className = "expandCard";
+//         a.className = "hide";
+//     } else if (checkbox2.checked == !true) {
+//         b.className = "card";
+//         a.className = "card";
+//     }
+// };
 
 function favoriteItem() {
     var favorite = document.getElementById('heart');
