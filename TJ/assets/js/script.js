28575dcc0ff64e6901463ef61a9748d13a48862d
TJ Momon <tjmomon@TJs-MacBook-Air.attlocal.net>
2023-01-18 15:38:01 -0600

    created framework for localstorage save on favorite button click

--- TJ/assets/js/script.js
+++ TJ/assets/js/script.js
@@ -1,3 +1,11 @@
+// Get the recipe data from local storage
+var recipeTitle = localStorage.getItem("recipeTitle");
+var recipeImage = localStorage.getItem("recipeImage");
+var recipeSourceUrl = localStorage.getItem("recipeSourceUrl");
+
+// Populate the foodRecipe div with the recipe data
+document.getElementById("card-populate1").innerHTML = "<h4>" + recipeTitle + "</h4><img src='" + recipeImage + "'><a href='" + recipeSourceUrl + "'>Go To Recipe</a>";
+
 window.addEventListener("load", function(){
     fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     .then(response => response.json())
@@ -10,15 +18,18 @@ window.addEventListener("load", function(){
         // Populate the drinkRecipe div with the cocktail data
         $("#card-populate2").html("<h4>" + cocktailName + "</h4><img src='" + cocktailImage + "'><a href='" + cocktailUrl + "'>Go To Recipe</a>");
     });
+    localStorage.removeItem("recipeTitle");
+    localStorage.removeItem("recipeImage"); 
+    localStorage.removeItem("recipeSourceUrl"); 
 });
 
-// Get the recipe data from local storage
-var recipeTitle = localStorage.getItem("recipeTitle");
-var recipeImage = localStorage.getItem("recipeImage");
-var recipeSourceUrl = localStorage.getItem("recipeSourceUrl");
+// // Get the recipe data from local storage
+// var recipeTitle = localStorage.getItem("recipeTitle");
+// var recipeImage = localStorage.getItem("recipeImage");
+// var recipeSourceUrl = localStorage.getItem("recipeSourceUrl");
 
-// Populate the foodRecipe div with the recipe data
-document.getElementById("card-populate1").innerHTML = "<h4>" + recipeTitle + "</h4><img src='" + recipeImage + "'><a href='" + recipeSourceUrl + "'>Go To Recipe</a>";
+// // Populate the foodRecipe div with the recipe data
+// document.getElementById("card-populate1").innerHTML = "<h4>" + recipeTitle + "</h4><img src='" + recipeImage + "'><a href='" + recipeSourceUrl + "'>Go To Recipe</a>";
 
 // function expandCard() {
 //     var checkbox = document.getElementById('expand');
@@ -49,8 +60,14 @@ document.getElementById("card-populate1").innerHTML = "<h4>" + recipeTitle + "</
 function favoriteItem() {
     var favorite = document.getElementById('heart');
     if (favorite.checked == true) {
+        var recipe = document.getElementById("card-populate1");
+        // var recipeUrl = 
+        localStorage.setItem("recipeName", JSON.stringify(recipe));
+        localStorage.setItem("recipeUrl", JSON.stringify(recipeUrl));
         console.log('success');
     } else if (favorite.checked == !true) {
+        localStorage.removeItem("recipeTitle");
+        localStorage.removeItem("recipeSourceUrl");
         console.log('also success');
     }
 };
@@ -58,8 +75,14 @@ function favoriteItem() {
 function favoriteItem2() {
     var favorite2 = document.getElementById('heart2');
     if (favorite2.checked == true) {
+        var cocktailName = document.getElementById("card-populate2");
+        var cocktailUrl = document.getElementById("a href");
+        localStorage.setItem("drinkName", JSON.stringify(cocktailName));
+        localStorage.setItem("drinkUrl", JSON.stringify(cocktailUrl));
         console.log('success');
     } else if (favorite2.checked == !true) {
+        localStorage.removeItem("drinkName");
+        localStorage.removeItem("drinkUrl");
         console.log('also success');
     }
 };
