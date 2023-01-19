d1153b933e17597e4bb214d9b11d7d19cbd2b6bb
TJ Momon <tjmomon@TJs-MacBook-Air.attlocal.net>
2023-01-18 19:56:13 -0600

    github troubleshooting

---
+++ joshua/assets/files/favorites.js
@@ -0,0 +1,19 @@
+// Get the 'card-section' div
+const cardSection = document.querySelector(".card-section");
+
+// Check if recipe info is stored in Local Storage
+if (localStorage.getItem("recipeSource") && localStorage.getItem("recipeSourceUrl")) {
+
+    // Get the recipe source URL and title from Local Storage
+    const recipeSourceUrl = localStorage.getItem("recipeSourceUrl");
+    const recipeTitle = localStorage.getItem("recipeTitle");
+   // Get the 'card-section' div on 'favorites.html'
+   const cardSection = document.querySelector(".card-section");
+   // Get existing articles in the 'card-section' div
+   const existingArticles = cardSection.querySelectorAll("article");
+   // Iterate through existing articles
+   existingArticles.forEach(function(article) {
+       // Assign the recipe source URL and title to the article
+       article.innerHTML = `<a href="${recipeSourceUrl}">${recipeTitle}</a>`;
+});
+}
